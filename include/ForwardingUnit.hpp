9616a0f88cf1884b5f833b02c8e0d86b0f8927c4
Pandu <karthiktamarapalli9@gmail.com>
2026-02-24 15:40:24 +0530

    All the interfaces for the simulator are added

---
+++ include/ForwardingUnit.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include "Pipeline.hpp"
+class ForwardingUnit{
+    public:
+        void resolveForwarding(const ID_EX& id_ex,const EX_MEM& ex_mem,const MEM_WB& mem_wb,int operand1,int operand2);  
+};
